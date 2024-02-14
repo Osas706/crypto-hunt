fb3226bc4f2884338eaa7fcbb3e1eaca936875fa
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-02-14 17:14:16 +0100

    home pagee

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, makeStyles } from '@material-ui/core';
+import { Container, Typography, makeStyles } from '@material-ui/core';
+import Carousel from './Carousel';
 
 const useStyles = makeStyles(() => ({
   banner: {
@@ -12,6 +13,14 @@ const useStyles = makeStyles(() => ({
     flexDirection: 'column',
     paddingTop: 25,
     justifyContent: 'space-around'
+  },
+
+  tagline: {
+    display: 'flex',
+    height: '40%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
   }
 }))
 
@@ -21,7 +30,31 @@ const Banner = () => {
   return (
     <div className={classes.banner}>
       <Container className={classes.banerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant='h2'
+            style={{
+              fontWeight: 'bold',
+              marginBottom: 15,
+              fontFamily: 'montserrat',
+            }}
+          >
+            Crypto Hunt
+          </Typography>
 
+          <Typography
+            variant='subtitle2'
+            style={{
+              color: 'darkgray',
+              textTransform: 'capitalize',
+              fontFamily: 'montserrat'
+            }}
+          >
+            Get all the info regarding your favourite Crypto Currency
+          </Typography>
+        </div>
+
+        <Carousel />
       </Container>
     </div>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
+import CoinsTable from '../components/CoinsTable';
 
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <CoinsTable />
     </>
   )
 }
